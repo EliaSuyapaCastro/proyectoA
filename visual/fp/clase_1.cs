094ca5be50d5b8bab524d572921ebc26dbddd500
EliaSuyapaCastro <eliacastro402@gmail.com>
2021-10-22 09:11:41 -0600

    actualizacion2

---
+++ visual/fp/clase_1.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Cllections.Generic;
+using System.Linq;
+using System.Tasks;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDPresentacion{
+
+    public class Class1{
+        
+    }
+}
